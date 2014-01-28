e54c5589f7feb37144cd696016c75e167ae3d73c
unknown <nf3jyl@D4295152.federated.fds>
2014-01-28 10:37:29 -0500

    toy gradle

--- src/main/java/org/myorg/gradle/example/simple/HelloWorld.java
+++ src/main/java/org/myorg/gradle/example/simple/HelloWorld.java
@@ -1,5 +1,10 @@
 package org.myorg.gradle.example.simple;
 
+/**
+ *
+ * Gradle Docs
+ *
+**/
 public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("hello gradle");
