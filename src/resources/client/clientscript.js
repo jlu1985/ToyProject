a23ac219dcb949562d18990a7a80634140e0fb37
Jia Lu <jlu1985@gmail.com>
2014-01-29 13:39:45 -0500

    client side - window object

---
+++ src/resources/client/clientscript.js
@@ -0,0 +1,12 @@
+//Html Embedded Script
+//<script> ... </script>
+
+//External Script
+//<script src="../../scripts/util.js"></script>
+
+//JavaScript in URLs
+// User javascript: protocol specifier
+//<a href="javascript:new Date().toLocalTimeString();">What time is it?</a>
+
+//Force the return value to be undefined by specifying void
+//<a href="javascript:void window.open('about:blank');">What time is it?</a>
---
+++ src/resources/client/window.js
@@ -0,0 +1,17 @@
+//Notable Window Object functions
+setTimer();
+setInterval();
+window.location
+
+//History
+window.history
+history.go(-2);
+
+//Navigator
+navigator.userAgent
+
+//Dialog Boxes
+window.prompt
+
+//Dialog popup
+window.showModelDialog()
