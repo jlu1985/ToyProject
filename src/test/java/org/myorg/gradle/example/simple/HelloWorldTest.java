75efe885df0b90785f8f799cd67877ea5c0fccd5
Jia Lu <jlu1985@gmail.com>
2014-02-25 14:53:33 -0500

    gradle config

---
+++ src/test/java/org/myorg/gradle/example/simple/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package org.myorg.gradle.example.simple;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	@Ignore("nothing to test")
+	public void testMain() {
+	}
+
+}
