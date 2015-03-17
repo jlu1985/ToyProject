eabb21b19bf1a8ed281cef16403c56ab2e17b2a7
Jia Lu <jlu1985@gmail.com>
2015-03-16 23:43:30 -0400

    jquery playground exercise two character count

--- exercises/jquery/playground/playground.js
+++ exercises/jquery/playground/playground.js
@@ -1,14 +1,18 @@
 /**
  * 
  */
+
+// Utility Functions begins
 function isNotNumber(value) {
 	return (isNaN(value) || value == null || value === '');
 }
 
+// Utility Functions Ends
+
+// Section one function: calculate total
 function calTotal() {
 	var result = $('#cal-result');
 	var inputs = $('#calculateTotal > input.cal-input')
-
 	var total = 0;
 
 	inputs.each(function(i, el) {
@@ -18,10 +22,33 @@ function calTotal() {
 	result.text(total);
 };
 
+// Section two function: Counting characters
+function getCount(field, callback) {
+	'use strick';
+	var $field = field;
+	var count = $field.val().length;
+	var limit = $field.attr('maxLength');
+	var $statusField = $field.parent().children('.commentbox-status');
+	callback($statusField, limit, count);
+}
+
+function updateCharLimit(field, limit, count) {
+	'use strick';
+	var $field = field;
+	var html = count + " of " + limit;
+	$field.text(html);
+}
+
+// On Load binding
 $(document).ready(function() {
 	'use strict';
 
 	$('#calculateTotal > input.cal-input').blur(calTotal);
 	$('#calculateTotal > input.cal-input').keyup(calTotal);
 	calTotal();
+
+	$('#count_remaining_char .commentbox').keyup(function() {
+		getCount($(this), updateCharLimit);
+	}).keyup();
+	
 });
